38ac4308568552509fddbd9745724622e2ef827a
Gregory Quezada <quezada.gregory@yahoo.com>
2022-06-06 20:13:35 -0400

    Modified app.js to to create simple GET request

--- app.js
+++ app.js
@@ -1,2 +1,10 @@
 var express = require("express");
 var app = express();
+
+app.get("/url", (req, res, next) => {
+    res.json(["Gregory", "Vee", "Mary", "Blonde", "Pastries"]);
+});
+
+app.listen(3000, () => {
+    console.log("Server running on port 3000");
+});
